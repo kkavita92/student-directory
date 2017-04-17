52c797c7b4167b754aa7b3492e57b0f0dbd7f1f7
Kavita K <kkavita92@gmail.com>
2017-04-18 03:41:10 +0800

    Breaks down code into shorter methods

--- Exercises/ex7.rb
+++ Exercises/ex7.rb
@@ -1,26 +1,30 @@
 COHORTS = [:january, :february, :march, :april, :may, :june, :july, :august, :september, :october, :november, :december]
 
-def input_students
+def input_students 
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  students = []
+  @students = []
   name = gets.chomp #get the first name
   while !name.empty? do
-    puts "Which cohort is #{name} from?"
-    cohort = gets.chomp
-    if COHORTS.include?(cohort.downcase.to_sym)
-      students << {name: name, cohort: cohort.downcase.to_sym}
-    else
-      puts "Incorrect input. We will assume cohort is unknown."
-      students << {name: name, cohort: "Unknown"}
-    end
-    singular_manager(students)
+    get_cohort(name)
+    singular(@students)
     name = gets.chomp #get another name from user
   end
-  students
+  @students
 end
 
-def singular_manager(names)
+def get_cohort(name) #get cohort as input
+  puts "Which cohort is #{name} from?"
+  @cohort = gets.chomp
+  if COHORTS.include?(@cohort.downcase.to_sym)
+    @students << {name: name, cohort: @cohort.downcase.to_sym}
+  else
+    puts "Incorrect input. We will assume cohort is unknown."
+    @students << {name: name, cohort: "Unknown"}
+  end
+end
+
+def singular(names)
   if names.size == 1
     puts "Now we have 1 student. Add more?"
   else
