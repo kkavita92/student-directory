bd112d3b74ce817b3d3c63abbd82de60ff91275a
Kavita K <kkavita92@gmail.com>
2017-04-19 12:42:55 +0800

    Adds method to save data to file

--- directory.rb
+++ directory.rb
@@ -4,6 +4,7 @@ COHORTS = [:january, :february, :march, :april, :may, :june, :july, :august, :se
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
@@ -20,10 +21,22 @@ def process(selection)
     students = input_students
   when "2"
     print_output
+  when "3"
+    save_students
   when "9"
   else
     puts "I don't know what you meant, try again"
   end
+end
+
+def save_students
+  file = File.open("students.csv","w")
+  @students.each do |student|
+    student_data = [student[:name],student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+    file.close
 end
 
 def input_students
